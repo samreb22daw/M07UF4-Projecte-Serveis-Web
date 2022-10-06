f7ef2fae88b3525ab93227ed48c54edc6266c8f5
samreb22daw <samreb22daw@proton.me>
2022-10-06 17:21:13 +0200

    Inici programa

--- 1-Rest_NodeJS_Express/app.js
+++ 1-Rest_NodeJS_Express/app.js
@@ -6,7 +6,49 @@
 */
 
 const express = require('express'); // 'const': Quiere decir que no se puede cambiar el contenido de la variable.
-const app=express();
+const app = express();
 
-app.use(express.urlencoded({extended: true})); // Quiere decir que queremos trabajar con URLs completas.
-app.use(express.json()) // // Para analizar las peticiones HTTP que lleven JSON en el body o cuerpo (le indicamos que queremos trabajar con JSONs).
+app.use(express.urlencoded({ extended: true })); // Quiere decir que queremos trabajar con URLs completas.
+app.use(express.json()) // Para analizar las peticiones HTTP que lleven JSON en el body o cuerpo (le indicamos que queremos trabajar con JSONs).
+
+
+let jugades = [
+    { jugada: 'pedra' },
+    { jugada: 'paper' },
+    { jugada: 'tisora' }
+];
+
+let codiPartida = [
+    { gameCode: 0 }
+];
+
+
+
+
+app.post('/iniciarJoc/codiPartida', (req, res) => {
+    let nouCodi = { gameCode: parseInt(req.body.gameCode) }; // req.body = sirve para rellenar y añadir nuevos 'campos'
+    for (let i = 0; i < codiPartida.length; i++) {
+        if (codiPartida[i].gameCode == nouCodi.gameCode) {
+            res.send(`El codi de partida està repetit. Introdueix un codi de partida diferent.`);
+            break;
+        }
+    }
+    codiPartida.push(nouCodi);
+    res.send(codiPartida);
+
+
+});
+
+app.get('/consultarEstatPartida/codiPartida : JSON', (req, res) => {
+
+});
+
+app.put('/moureJugador/codiPartida/jugador/tipusMoviment', (req, res) => {
+
+});
+
+app.delete('/acabarJoc/codiPartida', (req, res) => {
+
+});
+
+app.listen(3000, () => console.log('Inici del servidor.'));
