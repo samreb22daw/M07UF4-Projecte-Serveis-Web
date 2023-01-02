371c065a9ba2c1fe2cb4a9484d8522a676fdc18e
Xavi Aranda <xavara22daw@proton.me>
2023-01-02 16:59:16 +0100

    Funciones medio acabadas falta perfeccionar

--- 1-Rest_NodeJS_Express/app.js
+++ 1-Rest_NodeJS_Express/app.js
@@ -12,70 +12,116 @@ app.use(express.urlencoded({ extended: true })); // Quiere decir que queremos tr
 app.use(express.json()) // Para analizar las peticiones HTTP que lleven JSON en el body o cuerpo (le indicamos que queremos trabajar con JSONs).
 
 
-let jugades = [
-    { jugada: 'pedra' },
-    { jugada: 'paper' },
-    { jugada: 'tisora' }
-];
-
-
 let codisPartides = [
-];
-
-let prova = [
-
 ];
 
 app.post('/iniciarJoc/codiPartida/:gameCode', (req, res) => {
     // El código de la nueva partida es introducido desde los parámetros de la dirección. 
     let codiRepetit = false;
-    let codiNou = []; 
-    for (let i of codisPartides){
-        if (i.gameCode == req.params.gameCode){
+    let codiNou = [];
+    for (let i of codisPartides) {
+        if (i.gameCode == req.params.gameCode) {
             codiRepetit = true;
         }
     }
 
     // Revisión de la variable 'codiRepetit' para comprobar si el codigo introducido está repetido o no.
-    if (codiRepetit == true){
+    if (codiRepetit == true) {
         res.send("El codi de partida introduït està repetit. Introdueix un codi de partida diferent.");
-    }else {
-        codiNou = {gameCode: parseInt(req.params.gameCode), jugadaJugador1:'', jugadaJugador2:'', guanyadesJugador1:0,guanyadesJugador2:0};
+    } else {
+        codiNou = { gameCode: parseInt(req.params.gameCode), jugadaJugador1: '', jugadaJugador2: '', guanyadesJugador1: 0, guanyadesJugador2: 0 };
         codisPartides.push(codiNou);
         res.send(codisPartides);
     }
 });
 
 app.get('/consultarEstatPartida/:codiPartida', (req, res) => {
-    codisPartides.forEach(function(partida) {
+    codisPartides.forEach(function (partida) {
         if (partida.gameCode === parseInt(req.params.codiPartida)) {
-            res.send(partida);
+            let partidaActual = { gameCode: partida.gameCode, GuanyadesJugador1: partida.guanyadesJugador1, GuanyadesJugador2: partida.guanyadesJugador2 };
+            res.send(partidaActual);
         }
     })
-    
-    res.send()
+
+
 });
+
+app.put('/moureJugador/:codiPartida/:jugador/:jugada', (req, res) => {
+    codisPartides.forEach(function (partida) {
+        if (partida.gameCode === parseInt(req.params.codiPartida)) {
+            if (req.params.jugador == 1) {
+                partida.jugadaJugador1 = req.params.jugada;
+            } else if (req.params.jugador == 2) {
+                partida.jugadaJugador2 = req.params.jugada;
+            }
+        }
+    })
+
+    res.send(codisPartides);
+});
+
+app.put('/jugarPartida/:codiPartida', (req, res) => {
+    codisPartides.forEach(function (partida) {
+        if (partida.gameCode === parseInt(req.params.codiPartida)) {
+            eleccionJugador1 = partida.jugadaJugador1;
+            eleccionJugador2 = partida.jugadaJugador2;
+
+            if( eleccionJugador1 == '' && eleccionJugador2 == ''){
+                res.send ("Ningún jugador ha escollit moviment.");
+            }else if( eleccionJugador2 == ''){
+                res.send("El jugador 2 no ha escollit moviment.");
+            }else if( eleccionJugador1 == ''){
+                res.send("El jugador 1 no ha escollit moviment.");
+            }else if (partida.guanyadesJugador1 == 3 || partida.guanyadesJugador2 == 3){
+                res.send('La partida ha finalitzat. Has d\'acabar la partida manualment.');
+            }
+            else if (eleccionJugador1 == eleccionJugador2) {
+                res.send("Els jugadors han empatat la partida.");
+            } else if (
+                (eleccionJugador1 === 'pedra' && eleccionJugador2 === 'tisora') ||
+                (eleccionJugador1 === 'paper' && eleccionJugador2 === 'pedra') ||
+                (eleccionJugador1 === 'tisora' && eleccionJugador2 === 'paper')
+            ) {
+                partida.guanyadesJugador1 = partida.guanyadesJugador1 + 1;
+                if(partida.guanyadesJugador1 == 3){
+                    res.send("EL JUGADOR 1 HA GUANYAT LA PARTIDA!!!");
+                }else{
+                    res.send("El jugador 1 ha guanyat el torn.");
+                }
+                res.send("El jugador 1 ha guanyat el torn.");
+            } else {
+                partida.guanyadesJugador2 = partida.guanyadesJugador2 + 1;
+                if(partida.guanyadesJugador2 == 3){
+                    res.send("EL JUGADOR 2 HA GUANYAT LA PARTIDA!!!");
+                }else{
+                    res.send("El jugador 2 ha guanyat el torn.");
+                }
+                
+            }
+        }
+    })
+});
+
+
+
+    app.delete('/acabarJoc/:codiPartida', (req, res) => {
+
+        codisPartidesNou = [];
+
+        for (let i of codisPartides) {
+            if (i.gameCode != req.params.codiPartida) {
+                codisPartidesNou.push(i);
+            }
+        }
+
+        codisPartides = codisPartidesNou;
+
+        res.send(codisPartides);
+        
+    });
 
 app.get('/consultarServidor', (req, res) => {
     res.send("Servidor funcionant correctament!");
-
-});
-
-app.put('/moureJugador/:codiPartida/:jugador/:jugada', (req, res) => { 
-    let partidaActual = {codiPartida: parseInt(req.params.codiPartida), jugador: parseInt(req.params.jugador), jugada: req.params.jugada};
-    
-    prova.push(partidaActual);
-    for (let i of codisPartides){
-        if (i.gameCode == prova.codiPartida){
-            
-        }else{
-            //EL CODIGO NO EXISTE
-        }
-    }
-    res.send(prova);
-});
-
-app.delete('/acabarJoc/codiPartida', (req, res) => {
 
 });
 
