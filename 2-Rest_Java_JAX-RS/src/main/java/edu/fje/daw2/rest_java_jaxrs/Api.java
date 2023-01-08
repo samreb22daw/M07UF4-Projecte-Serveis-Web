b3bd3385525fe65972085626b16ffddcbfda7dbe
samreb22daw <samreb22daw@proton.me>
2023-01-08 18:27:31 +0100

    08/01/2023 ---> Se crean partidas (/iniciarJoc/codiPartida/{gameCode})

--- 2-Rest_Java_JAX-RS/src/main/java/edu/fje/daw2/rest_java_jaxrs/Api.java
+++ 2-Rest_Java_JAX-RS/src/main/java/edu/fje/daw2/rest_java_jaxrs/Api.java
@@ -1,12 +1,22 @@
 package edu.fje.daw2.rest_java_jaxrs;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/daw2")
 public class Api {
+
+    @Context
+    private UriInfo context;
+    // Array (vacío por el momento), donde guardaremos las partidas, movimientos de los jugadores y sus victorias
+    private static List<Partida> codisPartides = new ArrayList<Partida>();
+
 
     // CONSULTAR FUNCIONAMIENTO DEL SERVIDOR --> GET: Consultamos que el servidor está funcionando
     @Path("/consultarServidor")
@@ -14,6 +24,17 @@ public class Api {
     @Produces(MediaType.TEXT_PLAIN)
     public String servidorConsultat() {
         return "Servidor funcionant correctament!";
+    }
+
+
+    // INICIAR PARTIDA --> POST: Indicamos en la URL el código de la partida que queremos empezar
+    @Path("/iniciarJoc/codiPartida/{gameCode}")
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response iniciarJoc(@PathParam("gameCode") int gameCode){
+        codisPartides.add(new Partida(gameCode,"","",0,0));
+        return Response.status(200).entity("La partida s'ha creat correctament.").build();
     }
 
 }
---
+++ 2-Rest_Java_JAX-RS/src/main/java/edu/fje/daw2/rest_java_jaxrs/Partida.java
@@ -0,0 +1,72 @@
+package edu.fje.daw2.rest_java_jaxrs;
+
+public class Partida {
+    // Atributos
+    private int gameCode;
+    private String jugadaJugador1;
+    private String jugadaJugador2;
+    private int guanyadesJugador1;
+    private int guanyadesJugador2;
+
+    // Constructor con parámetros
+    public Partida(int gameCode, String jugadaJugador1, String jugadaJugador2, int guanyadesJugador1, int guanyadesJugador2) {
+        this.gameCode = gameCode;
+        this.jugadaJugador1 = jugadaJugador1;
+        this.jugadaJugador2 = jugadaJugador2;
+        this.guanyadesJugador1 = guanyadesJugador1;
+        this.guanyadesJugador2 = guanyadesJugador2;
+    }
+
+    // Métodos Getters y Setters
+    public int getGameCode() {
+        return gameCode;
+    }
+
+    public void setGameCode(int gameCode) {
+        this.gameCode = gameCode;
+    }
+
+    public String getJugadaJugador1() {
+        return jugadaJugador1;
+    }
+
+    public void setJugadaJugador1(String jugadaJugador1) {
+        this.jugadaJugador1 = jugadaJugador1;
+    }
+
+    public String getJugadaJugador2() {
+        return jugadaJugador2;
+    }
+
+    public void setJugadaJugador2(String jugadaJugador2) {
+        this.jugadaJugador2 = jugadaJugador2;
+    }
+
+    public int getGuanyadesJugador1() {
+        return guanyadesJugador1;
+    }
+
+    public void setGuanyadesJugador1(int guanyadesJugador1) {
+        this.guanyadesJugador1 = guanyadesJugador1;
+    }
+
+    public int getGuanyadesJugador2() {
+        return guanyadesJugador2;
+    }
+
+    public void setGuanyadesJugador2(int guanyadesJugador2) {
+        this.guanyadesJugador2 = guanyadesJugador2;
+    }
+
+    // Método toString
+    @Override
+    public String toString() {
+        return "Partida {" +
+                "gameCode=" + gameCode +
+                ", jugadaJugador1='" + jugadaJugador1 + '\'' +
+                ", jugadaJugador2='" + jugadaJugador2 + '\'' +
+                ", guanyadesJugador1=" + guanyadesJugador1 +
+                ", guanyadesJugador2=" + guanyadesJugador2 +
+                '}';
+    }
+}
