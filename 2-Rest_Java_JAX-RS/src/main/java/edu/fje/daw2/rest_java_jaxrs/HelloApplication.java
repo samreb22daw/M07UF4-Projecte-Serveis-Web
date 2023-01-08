f4af2e1011abd88260dd928ab46e0dc653c5ae80
samreb22daw <samreb22daw@proton.me>
2023-01-08 15:29:31 +0100

    08/01/2023 ---> Creada la parte 2 con Java

---
+++ 2-Rest_Java_JAX-RS/src/main/java/edu/fje/daw2/rest_java_jaxrs/HelloApplication.java
@@ -0,0 +1,9 @@
+package edu.fje.daw2.rest_java_jaxrs;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
