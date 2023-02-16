c548a0e6b7e1ce052a954ccb9729d7fa4d758cd4
samreb22daw <samreb22daw@proton.me>
2023-02-16 10:35:13 +0100

    ACTUALIZADO PARA gRAPHQL DEVUELVA MENSAJES

--- 3-GraphQL/app.js
+++ 3-GraphQL/app.js
@@ -38,16 +38,23 @@ type Query {
 }
 
 type Mutation {
-  crearPartida(gameCode: ID!): Partida
-  moureJugador(gameCode: ID!, jugada1: Jugades, jugada2: Jugades): Partida
-  jugarPartida(gameCode: ID!): Partida
-  esborrarPartida(gameCode: ID!): [Partida]
+  crearPartida(gameCode: ID!): String
+  moureJugador(gameCode: ID!, jugada1: Jugades, jugada2: Jugades): String
+  jugarPartida(gameCode: ID!): String
+  esborrarPartida(gameCode: ID!): String
 }
 `);
 
 
 // Definimos la raíz (rootValue) de nuestro proyecto, donde programamos los métodos utilizados en 'query' y 'mutation'
 const arrel = {
+    // CONSULTAR FUNCIONAMIENTO DEL SERVIDOR
+    consultaServidor: () => {
+      console.log("Estat del servidor consultat, tot funcionant correctament.");
+      return "Servidor funcionant correctament!";
+    },
+
+
     // CONSULTAMOS EL ESTADO DE TODAS LAS PARTIDAS DISPONIBLES
     consultaPartides: () => {
       // Mostramos las partidas disponibles por consola, y devolvemos en GraphQL el Array 'partides' con todas las partidas disponibles
@@ -56,11 +63,7 @@ const arrel = {
       return partides;
     },
 
-    consultaServidor: () => {
-      return "El servidor funciona correctament"
-    },
-
-
+    
     // CONSULTAMOS EL ESTADO DE UNA PARTIDA EN CONCRETO INDICADA POR EL USUARIO
     consultaPartida: ({gameCode}) => {
       // Guardamos en la variable 'index' la posición en el Array de la partida que queremos consultar
@@ -85,7 +88,7 @@ const arrel = {
       if (partidaTrobada != -1){
         // Si 'partidaTrobada' != -1, quiere decir que la partida ya existe, por tanto no cremos la partida de nuevo
         console.log(`La partida amb codi ${gameCode} ja existeix, per tant, no es pot crear de nou.`);
-        return null;
+        return `La partida amb codi ${gameCode} ja existeix, per tant, no es pot crear de nou.`;
       }else {
         // Si 'partidaTrobada' NO es != -1, quiere decir que no existe una partida con el código que ha indicado el usuario. Creamos la partida
         partidaCreada = {
@@ -98,7 +101,7 @@ const arrel = {
         }
         partides.push(partidaCreada);
         console.log(`Partida amb codi ${gameCode} creada correctament.`);
-        return partidaCreada;
+        return `La partida amb codi ${gameCode} ha estat creada correctament.`;
       }
     },
 
@@ -110,34 +113,34 @@ const arrel = {
       if (index == -1){
         // Si 'index' == -1 la partida no existe, por tanto, indicamos que no existe por consola y no permitimos lanzar jugada
         console.log(`La partida amb codi ${gameCode} no existeix. No es pot llançar moviment a una partida que no existeix.`);
-        return null;
+        return `La partida amb codi ${gameCode} no existeix. No es pot llançar moviment a una partida que no existeix.`;
       }else {
         // Comprobamos antes de nada, que ninguno de los dos jugadores haya ganado la partida, es decir, haya ganado 3 turnos
         if (partides[index].tornsGuanyats1 == 3){ 
           console.log(`La partida amb codi ${gameCode} ha finalitzat. El jugador 1 ha guanyat la partida, ha arribat a les 3 victòries. Ja no es pot llançar moviments ni jugar més torns, però pots consultar el resultat de la partida.`);
-          return partides[index];
+          return `La partida amb codi ${gameCode} ha finalitzat. El jugador 1 ha guanyat la partida, ha arribat a les 3 victòries. Ja no es pot llançar moviments ni jugar més torns, però pots consultar el resultat de la partida.`;
         }else if (partides[index].tornsGuanyats2 == 3){
           console.log(`La partida amb codi ${gameCode} ha finalitzat. El jugador 2 ha guanyat la partida, ha arribat a les 3 victòries. Ja no es pot llançar moviments ni jugar més torns, però pots consultar el resultat de la partida.`);
-          return partides[index];
+          return `La partida amb codi ${gameCode} ha finalitzat. El jugador 2 ha guanyat la partida, ha arribat a les 3 victòries. Ja no es pot llançar moviments ni jugar més torns, però pots consultar el resultat de la partida.`;
         }else {
           /* Una vez comprobado que la partida donde se quiere jugar existe, y nadie ha ganado esa partida por el momento,comprobamos 
           si la jugada que se ha indicado es la del Jugador 1 o Jugador 2, y asignamos esta jugada a la partida en el Array */
           if (!jugada1 && !jugada2){
-            console.log("Cap jugador ha llançat una jugada. S'ha d'indicar la jugada que llançarà el Jugador 1 o el Jugador 2.");
-            return null;
+            console.log("No has llançat cap jugada. S'ha d'indicar la jugada que llançarà el Jugador 1 o el Jugador 2.");
+            return "No has llançat cap jugada. S'ha d'indicar la jugada que llançarà el Jugador 1 o el Jugador 2.";
           }else if (!jugada1){
             partides[index].jugada2 = jugada2;
             console.log(`El jugador 2 ha llançat "${jugada2}".`);
-            return partides[index]; 
+            return `El jugador 2 ha llançat '${jugada2}'.`; 
           }else if (!jugada2){
             partides[index].jugada1 = jugada1;
             console.log(`El jugador 1 ha llançat "${jugada1}".`);
-            return partides[index]; 
+            return `El jugador 1 ha llançat '${jugada1}'.`; 
           }else if (jugada1 && jugada2){
             partides[index].jugada1 = jugada1;
             partides[index].jugada2 = jugada2;
             console.log(`El jugador 1 ha llançat "${jugada1}" | El jugador 2 ha llançat "${jugada2}".`);
-            return partides[index]; 
+            return `El jugador 1 ha llançat '${jugada1}' | El jugador 2 ha llançat '${jugada2}'.`; 
           }
         }
       }
@@ -151,7 +154,7 @@ const arrel = {
       if (index == -1){
         // Si index == -1, quiere decir que la partida con el código que hemos introducido no existe
         console.log(`La partida amb el codi de partida ${gameCode} no existeix. Introdueix un codi d'una partida existent per poder jugar-la.`);
-        return null;
+        return `La partida amb el codi de partida ${gameCode} no existeix. Introdueix un codi d'una partida existent per poder jugar-la.`;
       }else {
         // Almacenamos en dos variables diferentes las jugadas del jugador 1 y del jugador 2 de la partida que vamos a jugar
         eleccionJugador1 = partides[index].jugada1;
@@ -159,55 +162,65 @@ const arrel = {
         if (partides[index].tornsGuanyats1 == 3){ // Comprobamos que el jugador 1 no haya ganado tres rondas
           // En caso de que el jugador 1 haya ganado tres rondas, mostramos los resultados de la partida
           console.log(`La partida amb codi ${gameCode} ha finalitzat. El jugador 1 ha guanyat la partida, ha arribat a les 3 victòries. Ja no es pot llançar moviments ni jugar més torns, però pots consultar el resultat de la partida.`);
-          return partides[index];
+          return `La partida amb codi ${gameCode} ha finalitzat. El jugador 1 ha guanyat la partida, ha arribat a les 3 victòries. Ja no es pot llançar moviments ni jugar més torns, però pots consultar el resultat de la partida.`;
         }else if (partides[index].tornsGuanyats2 == 3){ // Comprobamos que el jugador 2 no haya ganado tres rondas
           // En caso de que el jugador 2 haya ganado tres rondas, mostramos los resultados de la partida
           console.log(`La partida amb codi ${gameCode} ha finalitzat. El jugador 2 ha guanyat la partida, ha arribat a les 3 victòries. Ja no es pot llançar moviments ni jugar més torns, però pots consultar el resultat de la partida.`);
-          return partides[index];
+          return `La partida amb codi ${gameCode} ha finalitzat. El jugador 2 ha guanyat la partida, ha arribat a les 3 victòries. Ja no es pot llançar moviments ni jugar més torns, però pots consultar el resultat de la partida.`;
         }else if (eleccionJugador1 == "" && eleccionJugador2 == ""){ // Comprobamos que los dos jugadores han indicado un movimiento. Si no han seleccionado movimiento los dos jugadores, no se puede jugar la partida
           console.log("Cap jugador ha escollit jugada. Per jugar un torn els jugadors han d'escollir una jugada.");
-          return null;
+          return "Cap jugador ha escollit jugada. Per jugar un torn els jugadors han d'escollir una jugada.";
         }else if (eleccionJugador1 == ""){
           console.log("El jugador 1 no ha escollit jugada. Per poder jugar un torn, els dos jugadors han d'escollir una jugada.");
-          return null;
+          return "El jugador 1 no ha escollit jugada. Per poder jugar un torn, els dos jugadors han d'escollir una jugada.";
         }else if (eleccionJugador2 == ""){
           console.log("El jugador 2 no ha escollit jugada. Per poder jugar un torn, els dos jugadors han d'escollir una jugada.");
-          return null;
+          return "El jugador 2 no ha escollit jugada. Per poder jugar un torn, els dos jugadors han d'escollir una jugada.";
         }else if (eleccionJugador1 == eleccionJugador2){ // Empezamos con la lógica del programa (analizar las jugadas de los jugadores y decir quien de los dos gana el turno)
           console.log(`Els jugados han empatat el torn, han tret la mateixa jugada. (Jugada Jugador 1: ${eleccionJugador1} | Jugada Jugador 2: ${eleccionJugador2})`);
           /* Igualamos a nada ('') las jugadas de los dos jugadores, para que de este modo en cada turno tengan que indicar que jugada van
           a sacar, del mismo modo que el juego original */
           partides[index].jugada1 = "";
           partides[index].jugada2 = "";
-          return partides[index]; 
+          return `Els jugados han empatat el torn, han tret la mateixa jugada. (Jugada Jugador 1: ${eleccionJugador1} | Jugada Jugador 2: ${eleccionJugador2})`; 
         }else if ( // Filtramos para comprobar si el jugador1 ha ganado al jugador2
           (eleccionJugador1 === 'pedra' && eleccionJugador2 === 'tisora') ||
           (eleccionJugador1 === 'paper' && eleccionJugador2 === 'pedra') ||
           (eleccionJugador1 === 'tisora' && eleccionJugador2 === 'paper')
         ){
           partides[index].tornsGuanyats1 += 1; // Si el jugador 1 ha ganado, sumamos al jugador 1 un turno ganado
-          console.log(`El jugador 1 ha guanyat el torn. (Jugada Jugador 1: ${eleccionJugador1} | Jugada Jugador 2: ${eleccionJugador2})`);
           if (partides[index].tornsGuanyats1 == 3){
             // Si después de ganar un turno, los turnos ganados del jugador 1 == 3, indicamos que ha ganado la partida en la posición del Array "guanyadorPartida"
             partides[index].guanyadorPartida = "Jugador 1";
+            partides[index].jugada1 = "";
+            partides[index].jugada2 = "";
+            console.log("EL JUGADOR 1 HA GUANYAT LA PARTIDA, AMB AQUEST TORN QUÈ HA GUANYAT, HA ARRIBAT A LES 3 VICTÒRIES. FELICITATS!. (Jugada Jugador 1: "+eleccionJugador1+" | Jugada Jugador 2: "+eleccionJugador2+")");
+            return "EL JUGADOR 1 HA GUANYAT LA PARTIDA, AMB AQUEST TORN QUÈ HA GUANYAT, HA ARRIBAT A LES 3 VICTÒRIES. FELICITATS!. (Jugada Jugador 1: "+eleccionJugador1+" | Jugada Jugador 2: "+eleccionJugador2+")";
+          }else {
+            /* Igualamos a nada ('') las jugadas de los dos jugadores, para que de este modo en cada turno tengan que indicar que jugada van
+            a sacar, del mismo modo que el juego original */
+            partides[index].jugada1 = "";
+            partides[index].jugada2 = "";
+            console.log(`El jugador 1 ha guanyat el torn. (Jugada Jugador 1: ${eleccionJugador1} | Jugada Jugador 2: ${eleccionJugador2})`);
+            return `El jugador 1 ha guanyat el torn. (Jugada Jugador 1: ${eleccionJugador1} | Jugada Jugador 2: ${eleccionJugador2})`; 
           }
-          /* Igualamos a nada ('') las jugadas de los dos jugadores, para que de este modo en cada turno tengan que indicar que jugada van
-          a sacar, del mismo modo que el juego original */
-          partides[index].jugada1 = "";
-          partides[index].jugada2 = "";
-          return partides[index]; 
         }else { // Si no ha ganado el jugador1 significa que ha ganado el jugador2
           partides[index].tornsGuanyats2 += 1; // Si el jugador 2 ha ganado, sumamos al jugador 2 un turno ganado
-          console.log(`El jugador 2 ha guanyat el torn. (Jugada Jugador 1: ${eleccionJugador1} | Jugada Jugador 2: ${eleccionJugador2})`);
           if (partides[index].tornsGuanyats2 == 3){
             // Si después de ganar un turno, los turnos ganados del jugador 2 == 3, indicamos que ha ganado la partida en la posición del Array "guanyadorPartida"
             partides[index].guanyadorPartida = "Jugador 2";
+            partides[index].jugada1 = "";
+            partides[index].jugada2 = "";
+            console.log("EL JUGADOR 2 HA GUANYAT LA PARTIDA, AMB AQUEST TORN QUÈ HA GUANYAT, HA ARRIBAT A LES 3 VICTÒRIES. FELICITATS!. (Jugada Jugador 1: "+eleccionJugador1+" | Jugada Jugador 2: "+eleccionJugador2+")");
+            return "EL JUGADOR 2 HA GUANYAT LA PARTIDA, AMB AQUEST TORN QUÈ HA GUANYAT, HA ARRIBAT A LES 3 VICTÒRIES. FELICITATS!. (Jugada Jugador 1: "+eleccionJugador1+" | Jugada Jugador 2: "+eleccionJugador2+")";
+          }else {
+            /* Igualamos a nada ('') las jugadas de los dos jugadores, para que de este modo en cada turno tengan que indicar que jugada van
+            a sacar, del mismo modo que el juego original */
+            partides[index].jugada1 = "";
+            partides[index].jugada2 = "";
+            console.log(`El jugador 2 ha guanyat el torn. (Jugada Jugador 1: ${eleccionJugador1} | Jugada Jugador 2: ${eleccionJugador2})`);
+            return `El jugador 2 ha guanyat el torn. (Jugada Jugador 1: ${eleccionJugador1} | Jugada Jugador 2: ${eleccionJugador2})`; 
           }
-          /* Igualamos a nada ('') las jugadas de los dos jugadores, para que de este modo en cada turno tengan que indicar que jugada van
-          a sacar, del mismo modo que el juego original */
-          partides[index].jugada1 = "";
-          partides[index].jugada2 = "";
-          return partides[index]; 
         }
       }
     },
@@ -219,14 +232,13 @@ const arrel = {
       let index = partides.findIndex(partida => partida.gameCode == gameCode);
       if (index == -1){
         // Si index == -1, indicamos por mensaje en la consola que la partida no existe y por tanto no se puede eliminar
-        console.log(`La partida amb codi ${gameCode} no existeix, per tant, no por ser esborrada.`);
-        return null;
+        console.log(`La partida amb codi ${gameCode} no existeix, per tant, no pot ser esborrada.`);
+        return `La partida amb codi ${gameCode} no existeix, per tant, no pot ser esborrada.`;
       }else {
         // Si la partida que queremos eliminar existe, la eliminamos del Array 
         partides.splice(index,1);
-        
         console.log(`La partida amb codi ${gameCode} ha sigut esborrada correctament.`);
-        return partides;
+        return `La partida amb codi ${gameCode} ha sigut esborrada correctament.`;
       }
     }
 };
